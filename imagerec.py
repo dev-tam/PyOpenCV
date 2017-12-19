e1d417f692a7922c0463878d50cfd20b0ddaf3ee
dev-tam <tamjid.mdh@gmail.com>
2017-12-19 06:59:48 +0600

    view image plot

--- imagerec.py
+++ imagerec.py
@@ -2,9 +2,12 @@
 # import Image
 from PIL import Image
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 i = Image.open('images/dot.png')
 iar = np.asarray(i)
 
-print iar
+# print iar
+
+plt.imshow(iar)
+plt.show()
